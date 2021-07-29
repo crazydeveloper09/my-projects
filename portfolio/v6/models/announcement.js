41d6570e3985028349ae7552b2c1e71ce4725f95
crazydeveloper09 <39824136+crazydeveloper09@users.noreply.github.com>
2021-07-29 12:58:45 +0200

    Delete portfolio/v6 directory

--- portfolio/v6/models/announcement.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const announcementSchema = new mongoose.Schema({
-    pl: String,
-    en: String
-})
-
-module.exports = mongoose.model("Announcement", announcementSchema);
--- portfolio/v6/models/exercise.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const exerciseSchema = new mongoose.Schema({
-    title: String,
-    profile: String,
-    link: String
-}); 
-
-module.exports = mongoose.model("Exercise", exerciseSchema);
